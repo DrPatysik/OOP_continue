dec7cb716724aca89aed50c74252764a06f7318c
PATYA <patysik@mail.ru>
2024-02-04 19:47:30 +0300

    создала класс RECT

---
+++ src/main/kotlin/OOP_continue.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val rect = Rect(3,7)
+    val rect1 = Rect()
+     rect1.createRect(2,8)
+   println( rect.perimeterRect())
+    println( rect.squareRect())
+
+}
---
+++ src/main/kotlin/Rect.kt
@@ -0,0 +1,17 @@
+open class Rect( var length:Int, var width:Int) {
+    constructor():this(0 , 0)
+
+    fun createRect(length: Int, width: Int){
+        println("Rectangle length is $length and width is $width ")
+    }
+
+    fun perimeterRect():Int {
+        val perimeter = ( this.length + this.width ) * 2
+        return perimeter
+    }
+
+    fun squareRect():Int {
+        val square = this.length * this.width
+        return square
+    }
+}
